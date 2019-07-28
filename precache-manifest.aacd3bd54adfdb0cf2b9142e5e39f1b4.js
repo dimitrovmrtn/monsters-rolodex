610d0d2bf5c3de8c67b85416e759a0383415e96f
Martin Dimitrov <dimitrovmrtn@gmail.com>
2019-07-28 18:14:13 +0300

    Updates

---
+++ precache-manifest.aacd3bd54adfdb0cf2b9142e5e39f1b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf93a7f0427f70f39ad3f38f4f4ab45b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e505ba4b5ebfddf6d3f2",
+    "url": "/monsters-rolodex/static/css/main.65970b2e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e505ba4b5ebfddf6d3f2",
+    "url": "/monsters-rolodex/static/js/main.a4acdeda.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
